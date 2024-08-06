38aac3ade896d8c94532a05b639bcd7a56c0e86e
Paulinha159 <paularebesco@gmail.com>
2024-08-06 11:27:10 -0300

    tenque terminar

--- corretora.js
+++ corretora.js
@@ -4,12 +4,51 @@ const db =[];
 
 let proxId = 1 ;
 
-const model = () => {
+const model = (id = proxId++ ) => {
     const nome = prompt ("Nome: ");
+
     if (nome != ""){
         return {
             id,
             nome,
         }
     }
+
+    console.log ("Dados inválidos")
+};
+
+const criar = () =>{
+    const novo = model
+
+
+
+
+
+
+}
+
+const index = () =>{
+    if (db.length ==0){
+        console.log("Nenhum parametro encontrado")
+    } else {
+ db.forEach(el => console.log(el))
+ return true
+}
+}
+const show = id =>db.find(el=> el.id == id);
+
+const update = () =>{
+    if(index()){
+        const id = parseInt(prompt("ID: "));
+     const indice = db. findIndex( el => el.id == id) 
+      if (indice != -1){
+        const novo = model ( id );
+        if(novo){
+            db[indice]= novo;
+            consol
+        }
+      }
+
+    }
+
 }
