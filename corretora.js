1c749109d2b1378a2acc9d77ec0cc8dfe86cd01f
Paulinha159 <paularebesco@gmail.com>
2024-08-06 09:00:59 -0300

    init commit

---
+++ corretora.js
@@ -0,0 +1,15 @@
+const prompt = require ("prompt-sync")();
+
+const db =[];
+
+let proxId = 1 ;
+
+const model = () => {
+    const nome = prompt ("Nome: "):
+    if (nome != ""){
+        return {
+            id,
+            nome,
+        }
+    }
+}
